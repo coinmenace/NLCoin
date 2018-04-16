941a1334268684c5c6c29ca458bae472861902dd
biddyweb <webframes@gmail.com>
2018-04-16 08:58:49 +0100

    Changed Address

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -124,11 +124,15 @@ public:
             assert(consensus.hashGenesisBlock == uint256S("0x00000e1728b630fd83aecbc51546c7915fffb7d3c897b5fd8c4b14043070b7f0"));
             assert(genesis.hashMerkleRoot == uint256S("0x33a98e8f8089165dc24358b01d52dd740011bdbffad052d51d3ac3588af2f487"));
 
-            vSeeds.push_back(CDNSSeedData("nlcoin1", "seed1.protoncoin.info"));
-            vSeeds.push_back(CDNSSeedData("nlcoin2", "seed2.protoncoin.info"));
-            vSeeds.push_back(CDNSSeedData("nlcoin3", "seed3.protoncoin.info"));
-            vSeeds.push_back(CDNSSeedData("nlcoin4", "seed4.protoncoin.info"));
-            vSeeds.push_back(CDNSSeedData("dnsseed", "dnsseed.nlcoin.info"));
+            //vSeeds.push_back(CDNSSeedData("nlcoin1", "seed1.protoncoin.info"));
+            //vSeeds.push_back(CDNSSeedData("nlcoin2", "seed2.protoncoin.info"));
+            //vSeeds.push_back(CDNSSeedData("nlcoin3", "seed3.protoncoin.info"));
+            //vSeeds.push_back(CDNSSeedData("nlcoin4", "seed4.protoncoin.info"));
+            //vSeeds.push_back(CDNSSeedData("dnsseed", "dnsseed.nlcoin.info"));
+            vSeeds.push_back(CDNSSeedData("protoncoin1", "node1.mobilipia.com"));
+            vSeeds.push_back(CDNSSeedData("protoncoin2", "node2.mobilipia.com"));
+            vSeeds.push_back(CDNSSeedData("protoncoin3", "node3.mobilipia.com"));
+            vSeeds.push_back(CDNSSeedData("dnsseed", "node1.mobilipia.com"));
 
             // Proton addresses start with 'P'
             base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,55);
