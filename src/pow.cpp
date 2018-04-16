3cb5035e56d8cdb3c647a2a2cf2410d4a9131a9f
biddyweb <webframes@gmail.com>
2018-04-16 13:04:15 +0100

    Modified Time

--- src/pow.cpp
+++ src/pow.cpp
@@ -254,7 +254,9 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
 
     // Check proof of work matches claimed amount
     if (UintToArith256(hash) > bnTarget)
-        LogPrintf("POW Hash block %s.\n", hash.GetHex());
+        LogPrintf("POW Hash 0 block %s.\n", hash.GetHex());
+        LogPrintf("POW Hash block %s.\n", UintToArith256(hash).GetHex());
+        LogPrintf("POW Hash block %s.\n", bnTarget.GetHex());
         return error("CheckProofOfWork(): hash doesn't match nBits");
 
     return true;
